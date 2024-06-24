5c84636e249de8c464bf705e644ecac37a54c769
Andrey Cheshko <andreycheshko@gmail.com>
2024-06-24 18:04:14 +0300

    add login form

---
+++ js/login-form.js
@@ -0,0 +1,43 @@
+//for form
+let loginText = document.querySelector(".login__form-top .login");
+let loginForm = document.querySelector("form.login");
+let loginBtn = document.querySelector("label.login");
+let signupBtn = document.querySelector("label.signup");
+let signupLink = document.querySelector(".login__form-signup-link");
+
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+signupLink.onclick = () => {
+  signupBtn.click();
+  return false;
+};
+
+//for form modal
+let openButton = document.querySelector(".login__form-open");
+let closeButton = document.querySelectorAll(".login__form-close");
+let formModal = document.querySelector(".form-modal");
+
+// Add a 'click' event for the щзут buttons
+openButton.addEventListener("click", function () {
+  formModal.classList.add("open");
+});
+
+// Add a 'click' event for the close buttons
+for (let i = 0; i < closeButton.length; i++) {
+  closeButton[i].addEventListener("click", function () {
+    formModal.classList.remove("open");
+  });
+}
+
+//Close the modal window when clicking outside it
+window.addEventListener("click", function (event) {
+  if (event.target === formModal) {
+    formModal.classList.remove("open");
+  }
+});
---
+++ js/main.js
@@ -0,0 +1,15 @@
+// arrow to top
+document.querySelector(".back-to-top").addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener("scroll", function () {
+  let scrolled = window.scrollY;
+
+  if (scrolled > 1950) {
+    document.querySelector(".back-to-top").classList.add("active");
+  } else {
+    document.querySelector(".back-to-top").classList.remove("active");
+  }
+});
